b5aab10898a8f89eb8e2b7571d38d3052bd2ab23
nicolecraven <craventhegreat@gmail.com>
2023-10-03 11:13:45 -0400

    finished lab

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+    
+let random = Math.floor(Math.random() * 10) + 1;
+console.log(random);
+
+
+
+let num3 = 17;
+let num4 = 13;
+//let mod = (num3 % num4) +4;
+//let mod = num3 % num4 === 4;
+//console.log(mod);
+
+
+let numbers = [ 10, 5, 12, 8, 20];
+let max = Math.max( 10, 5, 12, 8, 20);
+console.log(max);
+
+
+ 
+ 
+
+
+
+
+
+
+
+
+
+
